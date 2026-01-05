b1599b0b32b55e91e44dc01b0de86e9724086f65
Marianzzz <marian0412510301@gmail.com>
2026-01-05 20:48:27 +0200

    add navigation for sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,20 @@
-import { BarChart3 } from 'lucide-react'
+import { BarChart3, Package, Plus, Settings } from 'lucide-react'
+import Link from 'next/link'
 export default function SideBar({ currentPath = '/dashboard' }: { currentPath: string }) {
+    const navigation = [
+        {
+            name: 'Dashboard', href: '/dashboard', icon: BarChart3,
+        },
+        {
+            name: 'Iventory', href: '/inventory', icon: Package,
+        },
+        {
+            name: 'Add Product', href: '/add-product', icon: Plus,
+        },
+        {
+            name: 'Settings', href: '/settings', icon: Settings,
+        }
+    ]
     return <div className="fix left-0 top-0 bg-black text-white w-64 min-h-screen p-6 z-10">
         <div className="mb-8">
             <div className="flex items-center space-x-2 mb-4">
@@ -7,5 +22,18 @@ export default function SideBar({ currentPath = '/dashboard' }: { currentPath: s
                 <span className='text-lg font-semibold'>Iventory App</span>
             </div>
         </div>
+        <nav className='space-y-1'>
+            <div className='text-sm font-semibold text-gray-400 uppercase'>
+                Iventory
+            </div>
+            {navigation.map((item, key) => {
+                const IconComponent = item.icon;
+                const isActive = currentPath === item.href;
+                return <Link href={item.href} key={key} className={`flex items-center space-x-3 py-2 px-3 rounded-lg ${isActive ? 'bg-gray-400 text-gray-800' : 'hover:bg-gray-400 hover:text-gray-800 text-gray-300'}`}>
+                    <IconComponent className='w-5 h-5' />
+                    <span className='text-sm'>{item.name}</span>
+                </Link>
+            })}
+        </nav>
     </div>
 }
