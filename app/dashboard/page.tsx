3ad3ab1bb38b32c8530beb4c1155ecc85a2b901c
Marianzzz <marian0412510301@gmail.com>
2026-01-06 11:58:41 +0200

    add section key metrics

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,29 +1,36 @@
+import KeyMetrics from "@/components/dashboard/key-metrics";
 import SideBar from "@/components/sidebar";
 import { getCurrentUser } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 
 export default async function DashBoardPage() {
     const { id } = await getCurrentUser();
-    const totalProducts = await prisma.product.count({
-        where: { userId: id }
-    })
-    const lowStock = await prisma.product.count({
-        where: {
-            userId: id
-        }
-    })
+
+    const [totalProducts, lowStock, allProducts ] = await Promise.all([
+        await prisma.product.count({
+            where: { userId: id }
+        }),
+        await prisma.product.count({
+            where: {
+                userId: id,
+                lowStockAt: { not: null },
+                quantity: { lte: 5 }
+            }
+        }),
+        await prisma.product.findMany({
+            where: {
+                userId: id,
+            },
+            select: { price: true, quantity: true, createdAt: true }
+        })
+    ])
+   
     const recent = await prisma.product.findMany({
         where: {
             userId: id,
         },
         orderBy: { createdAt: 'desc' },
         take: 5,
-    })
-    const allProducts = await prisma.product.findMany({
-        where: {
-            userId: id,
-        },
-        select: { price: true, quantity: true, createdAt: true }
     })
     const totalValue = allProducts.reduce((sum, product) => sum + Number(product.price) * Number(product.quantity), 0)
     return <div className="min-h-screen bg-gray-50">
@@ -37,7 +44,10 @@ export default async function DashBoardPage() {
                     </div>
                 </div>
             </header>
-
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
+                <KeyMetrics totalProducts={totalProducts} totalValue={totalValue} lowStock={lowStock} />
+            </div>
+            
         </main>
     </div>
 }
