cfc86c27ef75a2c58f3d3879991c5db8b453cb61
Marianzzz <marian0412510301@gmail.com>
2026-01-08 10:05:13 +0200

    create a getUser function to check if the user is logged in, and also display the link

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,12 @@
 import { stackServerApp } from "@/stack/server";
 import { redirect } from "next/navigation";
 
+export async function getUser() {
+  return await stackServerApp.getUser();
+}
+
 export async function getCurrentUser() {
-  const user = await stackServerApp.getUser();
+  const user = await getUser(); 
   if (!user) {
     redirect("/sign-in");
   }
