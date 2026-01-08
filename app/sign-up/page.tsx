9fde5e005472af31cc56dcdd028abc39c9a01edb
Marianzzz <marian0412510301@gmail.com>
2026-01-08 21:03:05 +0200

    implement sign-up and configure custom sign-in and sign-up urls

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,9 +1,10 @@
-import { SignIn } from "@stackframe/stack";
+import { SignUp } from "@stackframe/stack";
 
-export default function SignInPage() {
+export default function SignUpPage() {
     return <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to-purple-100">
         <div className="max-w-md w-full space-y-6">
-            <SignIn />
+            <SignUp />
         </div>
     </div>
+
 }
